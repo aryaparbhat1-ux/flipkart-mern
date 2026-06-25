ffb939da51e1bbb0f9687d0dbc8ab9e164a086e3
aryaparbhat1-ux <aryaparbhat1@gmail.com>
2026-06-25 14:56:25 +0530

    Update error.js

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -4,32 +4,32 @@ module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal Server Error";
 
-    // mongodb id error
+    // Wrong MongoDB ObjectId
     if (err.name === "CastError") {
         const message = `Resource Not Found. Invalid: ${err.path}`;
-        err = new ErrorHandler(message, 400)
+        err = new ErrorHandler(message, 400);
     }
 
-    // mongoose duplicate key error
+    // Duplicate key error
     if (err.code === 11000) {
         const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
         err = new ErrorHandler(message, 400);
     }
 
-    // wrong jwt error
-    if (err.code === "JsonWebTokenError") {
-        const message = 'JWT Error';
+    // Invalid JWT
+    if (err.name === "JsonWebTokenError") {
+        const message = "JWT Error";
         err = new ErrorHandler(message, 400);
     }
 
-    // jwt expire error
-    if (err.code === "JsonWebTokenError") {
-        const message = 'JWT is Expired';
+    // Expired JWT
+    if (err.name === "TokenExpiredError") {
+        const message = "JWT is Expired";
         err = new ErrorHandler(message, 400);
     }
 
-    res.status(err.statusCode).json({
+    return res.status(err.statusCode || 500).json({
         success: false,
-        message: err.message,
+        message: err.message || "Internal Server Error",
     });
-}
+};

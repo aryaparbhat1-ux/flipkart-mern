bd05bbbdcacb58a743cc9257312ddeb6c0f4da73
aryaparbhat1-ux <aryaparbhat1@gmail.com>
2026-06-25 15:05:20 +0530

    Update errorHandler.js

--- backend/utils/errorHandler.js
+++ backend/utils/errorHandler.js
@@ -1,10 +1,31 @@
-class ErrorHandler extends Error {
-    constructor(message, statusCode) {
-        super(message);
-        this.statusCode = statusCode
+const ErrorHandler = require("../utils/errorHandler");
 
-        Error.captureStackTrace(this, this.constructor);
+module.exports = (err, req, res, next) => {
+    console.log("ERROR =>", err);
+
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "Internal Server Error";
+
+    if (err.name === "CastError") {
+        const message = `Resource Not Found. Invalid: ${err.path}`;
+        err = new ErrorHandler(message, 400);
     }
-}
 
-module.exports = ErrorHandler;
+    if (err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+        err = new ErrorHandler(message, 400);
+    }
+
+    if (err.name === "JsonWebTokenError") {
+        err = new ErrorHandler("JWT Error", 400);
+    }
+
+    if (err.name === "TokenExpiredError") {
+        err = new ErrorHandler("JWT is Expired", 400);
+    }
+
+    return res.status(err.statusCode || 500).json({
+        success: false,
+        message: err.message || "Internal Server Error",
+    });
+};
